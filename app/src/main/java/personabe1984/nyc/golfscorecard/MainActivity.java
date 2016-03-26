4405cdbcc1576c588ecbf4bbb60f2ae23ef4ffd7
personabe1984 <personabe1984@gmail.com>
2016-03-26 16:24:32 -0400

    base layout for score

---
+++ app/src/main/java/personabe1984/nyc/golfscorecard/MainActivity.java
@@ -0,0 +1,13 @@
+package personabe1984.nyc.golfscorecard;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
