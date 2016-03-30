0785baa9e45f32254d49a779e5e6c9d8c9b94edd
personabe1984 <personabe1984@gmail.com>
2016-03-29 21:36:19 -0400

    made Hole object

--- app/src/main/java/personabe1984/nyc/golfscorecard/MainActivity.java
+++ app/src/main/java/personabe1984/nyc/golfscorecard/MainActivity.java
@@ -4,10 +4,17 @@ import android.app.ListActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private Hole[] mHoles = new Hole[18];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initializing Holes
+        int strokes = 0;
+        for(int i= 0; i < mHoles.length; i++ ){
+            mHoles[i] =  new Hole("Hole " + (i + 1) + " :", strokes);
+        }
     }
 }
